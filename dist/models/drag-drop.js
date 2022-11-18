9650882cf20ecdf765e1a0bc528bd55ccd01667d
satokiy <syamada@netprotections.co.jp>
2022-11-18 23:31:08 +0900

    [add]webpackの追加

--- dist/models/drag-drop.js
+++
@@ -1,6 +0,0 @@
-/**
- * drag & drop
- * interfaceを定義することによって、実装を強制する。
- */
-export {};
-//# sourceMappingURL=drag-drop.js.map
---
+++ webpack.config.prod.js
@@ -0,0 +1,23 @@
+const CleanPlugin = require("clean-webpack-plugin");
+const path = require("path");
+module.exports = {
+  mode: "production",
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+};
