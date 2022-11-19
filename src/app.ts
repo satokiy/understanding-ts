263d08a089a149a75350d67fb97e899cebc72529
satokiy <syamada@netprotections.co.jp>
2022-11-19 15:57:51 +0900

    [mod]サードパーティライブラリの利用

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
+import { plainToClass }from 'class-transformer';
+import 'reflect-metadata';
+import _ from "lodash";
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
+
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
+
+console.log(_.shuffle([1, 2, 3, 4]));
