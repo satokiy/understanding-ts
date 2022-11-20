05dddec03674b454505b0dc58093b9acadcf53bd
satokiy <syamada@netprotections.co.jp>
2022-11-21 00:26:29 +0900

    google mapの表示の途中

--- section13/src/app.ts
+++ section13/src/app.ts
@@ -1,2 +1,43 @@
-// ここにコードを書きます
-console.log('aaaaa')
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+const key = process.env.GOOGLE_API_KEY;
+
+document.addEventListener("DOMContentLoaded", function () {
+  let script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${key}`;
+  document.head.appendChild(script);
+});
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: string; lng: string } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  console.log(enteredAddress);
+
+  // google api の実行
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${key}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("座標を取得できませんでした。");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
--- section13/webpack.config.js
+++ section13/webpack.config.js
@@ -1,12 +1,15 @@
-const path = require('path');
+const path = require("path");
+const dotenv = require("dotenv");
+const env = dotenv.config().parsed;
+const webpack = require("webpack");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.ts',
+  mode: "development",
+  entry: "./src/app.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: './dist',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "./dist",
   },
   devServer: {
     static: [
@@ -20,17 +23,32 @@ module.exports = {
       },
     ],
   },
-  devtool: 'eval',
+  devtool: "eval",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(env),
+    }),
+  ],
+  resolve: {
+    fallback: {
+      http: require.resolve("stream-http"),
+      stream: require.resolve("stream-browserify"),
+      https: require.resolve("https-browserify"),
+      zlib: require.resolve("browserify-zlib"),
+      //      stream: false,
+      buffer: require.resolve("buffer/"),
+    },
   },
 };
